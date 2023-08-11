27230ad01086ea43e872b09e4201d3b7d21db3d3
Kwame Osei Bonsu <naasei41@gmail.com>
2023-08-11 21:47:29 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /*
- * main - Entry point
- *
- * Return : Always 0 (Success)
+ * Main - Entry point
+ * Return : Always Successful
  */
-int main(void)
+int main()
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return 0;
 }
