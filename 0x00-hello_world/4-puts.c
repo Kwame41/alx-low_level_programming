4245b61b7d7c45d41a4694d2d9d5a6d6ea65cc9d
Kwame41 <naasei41@gmail.com>
2023-08-12 04:54:36 -0700

    compiler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,10 @@
 #include<stlib.h>
 #include<time.h>
 /* Description of the function is for finding the exact words given*/
+
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
