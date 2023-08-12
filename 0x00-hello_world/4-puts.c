34c68ca3edcc986662d29167997ebd2378376610
Kwame41 <naasei41@gmail.com>
2023-08-12 03:40:58 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include<stlib.h>
 #include<time.h>
+/* Description of the function is for finding the exact words given*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
