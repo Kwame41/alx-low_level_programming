b39238c2f6362cbf325faa9e930aa70772df40d5
Kwame41 <naasei41@gmail.com>
2023-08-12 02:27:01 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
-/*
-*Main-Entry point
-*Return:Always successful
-*/
+#include <stdi0.h>
+#include<stlib.h>
+#include<time.h>
+
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+{       puts("\"Programming is like building a multilingual puzzle");
+        return(0);
 }	
