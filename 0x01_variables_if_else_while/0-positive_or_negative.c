8744b66d8f107e1d8e762bd7a606d083a53fe4ee
Kwame41 <naasei41@gmail.com>
2023-08-11 21:26:53 -0700

    My first code

---
+++ 0x01_variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+		printf("Then this (%d,n) is positive\n")
+	else if(n==0)
+		printf("Then this (%d,n) is zero\n")
+	else
+		printf("Truly this (%d,n) is negative\n")
+	return 0;
+}
