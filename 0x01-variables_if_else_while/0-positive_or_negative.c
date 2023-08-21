819ccd6b970204e6777b1383c85e2aa1c2ca7ebe
Kwame41 <naasei41@gmail.com>
2023-08-21 15:30:18 -0700

    digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,15 +3,17 @@
 #include<stdio.h>
 
 /**
- * main - Entry point 
- * Description: The randomized number is checked to see the type of integer.
- * Return: Always 0 (Success)
+ *main-Entry point
+ *Description:The randomized number is checked to see the type of integer.
+ *Return:Always 0(Success)
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
