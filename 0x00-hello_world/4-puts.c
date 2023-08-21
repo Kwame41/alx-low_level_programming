e51deb8c8eaa1cacf3126b01cb073dd368c47db6
Kwame41 <naasei41@gmail.com>
2023-08-21 07:05:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlb.h>
+
 /**
  * main - Entry point
- *
- * Description - This is the warning / reminder.
- *
- * return:  always void(success); when all is well.
+ * Description: void means it should not have any value or empty
+ * Return: Always void (success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
