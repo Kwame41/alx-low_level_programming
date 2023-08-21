334e146002e5141a1a554922144df4399e3c93fc
Kwame41 <naasei41@gmail.com>
2023-08-21 06:36:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - This is the warning / reminder.
+ *
+ * return:  always void(success); when all is well.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
