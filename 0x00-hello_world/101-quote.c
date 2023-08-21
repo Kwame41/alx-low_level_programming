543f452f39cda6b169d4f5951c252cec01df097b
Kwame41 <naasei41@gmail.com>
2023-08-21 11:42:55 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: void means it should not have any value or empty
+ * Return: Always void (success)
+ */
+
+int main(void)
+{
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
+        return (1);
+}
