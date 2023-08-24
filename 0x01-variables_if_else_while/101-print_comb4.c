5c8d2a9d63348c279702f0e3eb5816f9055d518c
Kwame Osei Bonsu <naasei41@gmail.com>
2023-08-24 17:00:46 +0000

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,19 +11,20 @@ int main(void)
 	{
 		for (y = x + 1; y < 9; y++)
 		{
-      for (z=y+1; z < 10; z++)
-        {
-          putchar((x % 10) + '0');
-			    putchar((y % 10) + '0');
-          putchar((z % 10) + '0');
+      			for (z=y+1; z < 10; z++)
+        		{
+          			putchar((x % 10) + '0');
+			    	putchar((y % 10) + '0');
+          			putchar((z % 10) + '0');
           
-          if (x == 7 && y == 8 && z==9)
-            continue;
-          putchar(',');
-			    putchar(' ');
+          			if (x == 7 && y == 8 && z==9)
+            				continue;
+          			putchar(',');
+			    	putchar(' ');
+			}
 		}
 	}
-
-putchar('\n');
-return (0);
+	
+ 	putchar('\n');
+	return (0);
 }
